b1a0df3d10029b2c7b2dde173b1f2fe71bf97945
Clarity <enaalmuhammad@gmail.com>
2024-05-16 10:27:00 +0700

    Update input-label.blade.php
    Font Style

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-bold  text-white']) }}>
+<label {{ $attributes->merge(['class' => 'block font-bold  text-white tracking-wider']) }}>
     {{ $value ?? $slot }}
 </label>
