4f246fca2f325941218fc0b3583817b6b70e3a6e
Clarity <enaalmuhammad@gmail.com>
2024-05-16 10:27:38 +0700

    Edit Shadow Field
    Gapake Shadow

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -2,5 +2,5 @@
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
     'class' => 'bg-white-custom border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 rounded-md shadow-sm mt-6',
-    'style' => 'box-shadow: inset 0 2px 4px rgba(0,0,0,0.5);'  // Inner shadow style
+    'style' => ';'  // Inner shadow style
 ]) !!}>
