6052bb7b1dffcf38f094f43a700177928216721c
Clarity <enaalmuhammad@gmail.com>
2024-05-16 10:30:19 +0700

    Tambah Font
    Font Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,22 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         "./node_modules/flowbite/**/*.js"
-        
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
             },
             colors: {
-                'custom-beige': '#F5F5F5', // Pastikan tidak mendefinisikan key warna yang sama lebih dari sekali. Ambil nilai yang Anda inginkan.
-                'green': '#87A922', // Warna untuk tombol
-                'orange': '#FC4100', // Warna hover untuk tombol
-                'white-custom' :'#F5F7F8',
+                'custom-beige': '#F5F5F5', 
+                'green': '#87A922', 
+                'orange': '#FC4100', 
+                'white-custom': '#F5F7F8',
             },
         },
     },
     plugins: [
-        // ...
         require('@tailwindcss/forms'),
         require('flowbite/plugin'),
-      ],
+    ],
 };
